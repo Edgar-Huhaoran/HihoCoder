b8f39f897ac74fdf7ffbb1353ec6345d119a0918
Edgar-Huhaoran <edgarhuhaoran@gmail.com>
2016-07-18 18:42:51 +0800

    finish Question C in hohiCoder race 9

---
+++ src/main/java/edgarhu/hihocoder/competition/race9/B/Main.java
@@ -0,0 +1,59 @@
+package edgarhu.hihocoder.competition.race9.B;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static final long MAX_N = 1000000000000000000L;
+
+    public static void main(String[] args) {
+        long[] boundArr = getBound(MAX_N);
+        long n;
+        Scanner in = new Scanner(System.in);
+
+        n = in.nextLong();
+        System.out.println(getNumber(boundArr, n));
+    }
+
+    /**
+     * 先找到原始数字和原始数字对应的位置,然后读取这一位
+     * @param boundArr
+     * @param n
+     * @return
+     */
+    public static int getNumber(long[] boundArr, long n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        int i = 1;
+        while (boundArr[i] < n) {
+            i++;
+        }
+
+        long temp = n - boundArr[i - 1] - 1;
+        long originNum = temp / i + (long)Math.pow(10, i - 1);
+        long bitPos = temp % i;
+
+        return (int)(originNum / (long)(Math.pow(10, i - 1 - bitPos)) % 10);
+    }
+
+    /**
+     * 获取不同位数数字之间的阀值
+     * @param max
+     * @return
+     */
+    public static long[] getBound(long max) {
+        long[] boundArr = new long[20];
+        int i = 1;
+        long factor = 9;
+        boundArr[0] = 0;
+        while (boundArr[i - 1] < max) {
+            boundArr[i] = boundArr[i - 1] + factor * i;
+            factor *= 10;
+            i++;
+        }
+        return boundArr;
+    }
+
+}
